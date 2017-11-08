b0b86f33e7fb809cfd607322b783f6c2972f4f65
Natasha Robarge <robarge.natasha7@gmail.com>
2017-11-07 18:55:39 -0600

    Took out background image

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,7 +7,7 @@ class Welcome extends Component {
         <div className="Titles welcome">
           <h1>Natasha Robarge</h1>
           <hr className="line-rule" />
-          <h4>Junior Software Developer</h4>
+          <h4>Junior Web Developer</h4>
         </div>
         <p className="Welcome-quote">"The number one benefit of information technology is that it empowers people to do what they want to do. It lets people be creative. It lets people be productive. It lets people learn things they didn't think they could learn before, and so in a sense it is all about potential. " </p>
         <p className="Quote-author"> -  Steve Ballmer </p>
