91f94cf6c5f7ee1eb630b212c9136186491313dc
Natasha Robarge <robarge.natasha7@gmail.com>
2017-11-15 19:40:48 -0600

    added better styling to media links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Link,
   Switch
 } from 'react-router-dom';
+import Favicon from 'react-favicon';
 import Welcome from './components/Welcome.js';
 import About from './components/About.js';
 import Projects from './components/Projects.js';
@@ -31,6 +32,15 @@ class App extends Component {
             </Switch>
           </div>
         </Router>
+        <footer>
+          <p>Check out my online presence!</p>
+          <div className="icon-container">
+            <a href="https://github.com/natasha-robarge"><img className="github" src="https://imageog.flaticon.com/icons/png/512/3/3641.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt="github logo" /></a>
+            <a href="https://www.linkedin.com/in/natasha-robarge/"><img className="linkedIn" src="https://image.flaticon.com/icons/svg/174/174857.svg" alt="LinkedIn logo" /></a>
+            <a href="https://twitter.com/NatTheCoderBat"><img className="twitter" src="http://www.twittericon.com/wp-content/uploads/2010/08/glossy-cute-blue1.png" alt="Twitter logo" /></a>
+            <a href="https://medium.com/@natasharobarge"><img className="medium" src="http://www.iconninja.com/files/757/850/542/medium-blog-icon.png" alt="Medium logo" /></a>
+          </div>
+        </footer>
       </div>
     );
   }
