c929d3869cd811c07ece5b4538c1d110891e3e12
Natasha Robarge <robarge.natasha7@gmail.com>
2017-11-18 14:56:06 -0600

    Have styled most of about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,24 +7,34 @@ class About extends Component {
         <div className="about-container">
           <div className="Titles about">
             <h1 className="statement">I Am a Full Stack Developer</h1>
+            <hr className="line-rule" />
           </div>
           <div className="songs">
               <h2>Coding Songs for Coding</h2>
-              <hr className="line-rule" />
               <iframe src="https://www.youtube.com/embed/b-Cr0EWwaTk" frameborder="0" gesture="media" allowfullscreen></iframe>
               <iframe src="https://www.youtube.com/embed/Wm2h0cbvsw8" frameborder="0" gesture="media" allowfullscreen></iframe>
               <iframe src="https://www.youtube.com/embed/jItnCGRsMjw" frameborder="0" gesture="media" allowfullscreen></iframe>
           </div>
-            <br />
           <div className="about-text">
-            {/* <img className="talk-photo" src="https://i.imgur.com/sUeWzze.jpg" /> */}
+            <hr />
+            <h2>Summary</h2>
+            <hr />
+            <br />
+            <br />
+            <img className="talk-photo" src="https://i.imgur.com/sUeWzze.jpg" />
             <p>I am the protagonist of a video game who is passionate and determined.
             I am a programmer who aspires to learn more about the field and how I can contribute in a meaningful 
             way and help others. As a humanitarian, I want to help society and I will do that through coding; whether 
             that means creating an informative website to teach people about different topics or by developing a video game 
             for people to play after a hard day. If it can help someone somehow, I'm in. </p>
+            <br />
+            <br />
+            <p>SIDENOTE: I also enjoy making occassionally bad coding puns because...</p>
+            <h5 className="pun">A smile a day keeps the bugs away</h5>
+          
           </div>
           <div className="skill-wrap">
+            <hr />
             <h2>Skills</h2>
             <hr />
             <br />
